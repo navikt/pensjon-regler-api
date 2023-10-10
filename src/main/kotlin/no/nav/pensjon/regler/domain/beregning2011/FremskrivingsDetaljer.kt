51b2e83408d043a01c6265db81f432afa584a896
Hellerslien, Erik <Erik.Hellerslien@nav.no>
2023-10-10 11:32:42 +0200

    Default constructor

--- src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/FremskrivingsDetaljer.kt
+++ src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/FremskrivingsDetaljer.kt
@@ -3,11 +3,10 @@ package no.nav.pensjon.regler.domain.beregning2011
 import java.io.Serializable
 import java.util.*
 
-class FremskrivingsDetaljer : Serializable, IFremskriving {
+class FremskrivingsDetaljer() : Serializable, IFremskriving {
     override var justeringTomDato: Date? = null
     override var justeringsfaktor = 0.0
     override var teller = 0.0
     override var nevner = 0.0
     var arskull = 0
-
 }
