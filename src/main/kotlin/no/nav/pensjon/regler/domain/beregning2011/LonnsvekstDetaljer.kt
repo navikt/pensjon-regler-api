d4a2e453d4a61d055fe378b6e213c0bfc4cc9c63
Hellerslien, Erik <Erik.Hellerslien@nav.no>
2023-10-10 12:11:50 +0200

    Copy Constructor lonnsvekstdetaljer

--- src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/LonnsvekstDetaljer.kt
+++ src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/LonnsvekstDetaljer.kt
@@ -7,4 +7,23 @@ class LonnsvekstDetaljer : Serializable, ILonnsvekst {
     override var justeringTomDato: Date? = null
     override var justeringsfaktor = 0.0
     override var lonnsvekst = 0.0
+
+    constructor()
+
+    constructor(lvd: LonnsvekstDetaljer) : this() {
+        if (lvd.justeringTomDato != null) {
+            justeringTomDato = lvd.justeringTomDato!!.clone() as Date
+        }
+        justeringsfaktor = lvd.justeringsfaktor
+        lonnsvekst = lvd.lonnsvekst
+    }
+
+    constructor(
+        justeringTomDato: Date? = null,
+        justeringsfaktor: Double = 0.0,
+        lonnsvekst: Double = 0.0) {
+        this.justeringTomDato = justeringTomDato
+        this.justeringsfaktor = justeringsfaktor
+        this.lonnsvekst = lonnsvekst
+    }
 }
