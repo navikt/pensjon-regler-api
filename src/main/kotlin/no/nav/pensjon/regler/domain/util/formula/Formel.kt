a58ae09bb4fc4ce7d2591dda0ec34461afd9dc65
Schiefloe, Jens <jens.schiefloe@nav.no>
2024-03-21 12:13:34 +0100

    stusset litt på formel

--- src/main/kotlin/no/nav/pensjon/regler/domain/util/formula/Formel.kt
+++ src/main/kotlin/no/nav/pensjon/regler/domain/util/formula/Formel.kt
@@ -2,7 +2,7 @@ package no.nav.pensjon.regler.domain.util.formula
 
 import java.io.Serializable
 
-class Formel : Serializable {
+class Formel() : Serializable {
     var emne: String = ""
     var navn: String = ""
     var prefix: String = ""
@@ -10,4 +10,15 @@ class Formel : Serializable {
     var notasjon: String = ""
     var innhold: String = ""
     var subFormelList: ArrayList<Formel> = ArrayList()
+
+    /** Copy constructor */
+    constructor(f: Formel) : this() {
+        emne = f.emne
+        navn = f.navn
+        prefix = f.prefix
+        resultat = f.resultat
+        notasjon = f.notasjon
+        innhold = f.innhold
+        subFormelList = ArrayList(f.subFormelList)
+    }
 }
