d3ea08c49577806ad65bf2b814ef62d43d4d6fc0
ehellerslien <erik.hellerslien@nav.no>
2024-04-30 13:33:47 +0200

    Bruker pom som versjon

--- releaseGithub.sh
+++ releaseGithub.sh
@@ -6,8 +6,8 @@ COMMIT=$(git rev-parse --short=12 HEAD)
 VERSION="1.$TIME-$COMMIT"
 echo "Setting version $VERSION"
 
-mvn -B versions:set -DnewVersion="$VERSION"
-mvn -B versions:commit
+##mvn -B versions:set -DnewVersion="$VERSION"
+##mvn -B versions:commit
 
 echo "Running release"
 mvn -B --settings maven-settings.xml deploy -Dmaven.wagon.http.pool=false
