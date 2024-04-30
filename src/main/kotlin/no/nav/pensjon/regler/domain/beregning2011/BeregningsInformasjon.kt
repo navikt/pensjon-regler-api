a583034c403fe80d07b2b7303bb6d21f05bc7b3d
Schiefloe, Jens <jens.schiefloe@nav.no>
2024-04-30 09:53:50 +0200

    issue-972: Slettet interfacer IBeregningsInformasjon og tilhørende.

--- src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/BeregningsInformasjon.kt
+++ src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/BeregningsInformasjon.kt
@@ -1,7 +1,5 @@
 package no.nav.pensjon.regler.domain.beregning2011
 
-import com.fasterxml.jackson.annotation.JsonSubTypes
-import com.fasterxml.jackson.annotation.JsonTypeInfo
 import no.nav.pensjon.regler.domain.Merknad
 import no.nav.pensjon.regler.domain.PenPerson
 import no.nav.pensjon.regler.domain.beregning.Sluttpoengtall
@@ -11,52 +9,38 @@ import no.nav.pensjon.regler.domain.kode.LandCti
 import no.nav.pensjon.regler.domain.kode.ResultatTypeCti
 import java.io.Serializable
 
-
-@JsonSubTypes(
-    JsonSubTypes.Type(value = BeregningsInformasjon::class),
-    JsonSubTypes.Type(value = IBeregningsInformasjon2025::class),
-    JsonSubTypes.Type(value = IBeregningsInformasjon2011::class)
-)
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-open class BeregningsInformasjon : Serializable, IBeregningsInformasjon2011 {
-    //Fra IBeregningsInformasjon2011
-    override var forholdstallUttak = 0.0
-    override var forholdstall67 = 0.0
+class BeregningsInformasjon : Serializable {
+    var forholdstallUttak = 0.0
+    var forholdstall67 = 0.0
     var delingstallUttak = 0.0
     var delingstall67 = 0.0
-    override var spt: Sluttpoengtall? = null
-    override var opt: Sluttpoengtall? = null
-    override var ypt: Sluttpoengtall? = null
-    override var grunnpensjonAvkortet = false
+    var spt: Sluttpoengtall? = null
+    var opt: Sluttpoengtall? = null
+    var ypt: Sluttpoengtall? = null
+    var grunnpensjonAvkortet = false
     var merknadListe: List<Merknad> = mutableListOf()
-    override var mottarMinstePensjonsniva = false
-    override var minstepensjonArsak: String? = null
-    override var rettPaGjenlevenderett = false
-    override var gjenlevenderettAnvendt = false
-    override var avdodesTilleggspensjonBrukt = false
-    override var avdodesTrygdetidBrukt = false
-    override var ungUfor = false
-    override var ungUforAnvendt = false
-    override var yrkesskadeRegistrert = false
-    override var yrkesskadeAnvendt = false
-    override var yrkesskadegrad = 0
-
-    // Fra IBeregningsInformasjon
-    override var penPerson: PenPerson? = null
-
-    // Fra IBeregningsInformasjon
-    override var beregningsMetode: BeregningMetodeTypeCti? = null
-    override var eksport = false
-    override var resultatType: ResultatTypeCti? = null
+    var mottarMinstePensjonsniva = false
+    var minstepensjonArsak: String? = null
+    var rettPaGjenlevenderett = false
+    var gjenlevenderettAnvendt = false
+    var avdodesTilleggspensjonBrukt = false
+    var avdodesTrygdetidBrukt = false
+    var ungUfor = false
+    var ungUforAnvendt = false
+    var yrkesskadeRegistrert = false
+    var yrkesskadeAnvendt = false
+    var yrkesskadegrad = 0
+    var penPerson: PenPerson? = null
+    var beregningsMetode: BeregningMetodeTypeCti? = null
+    var eksport = false
+    var resultatType: ResultatTypeCti? = null
     var tapendeBeregningsmetodeListe: List<TapendeBeregningsmetode> = mutableListOf()
-    override var trygdetid: Int? = null
-    override var tt_anv = 0
-    override var vurdertBosattland: LandCti? = null
-    override var ensligPensjonInstOpph = false
-    override var instOppholdType: JustertPeriodeCti? = null
-    override var instOpphAnvendt = false
-
-    // Avdødes tilleggspensjon
+    var trygdetid: Int? = null
+    var tt_anv = 0
+    var vurdertBosattland: LandCti? = null
+    var ensligPensjonInstOpph = false
+    var instOppholdType: JustertPeriodeCti? = null
+    var instOpphAnvendt = false
     var tp = 0.0
     var ttBeregnetForGrunnlagsrolle = 0
     var ungUforGarantiFrafalt = false
