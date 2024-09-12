439681b2c635aafdeac2d2dc2e02adb1e37ee746
ehellerslien <erik.hellerslien@nav.no>
2024-09-12 11:15:44 +0200

    issue-970: Sanerer request- og response objekter for BeregnOpptjening og RegulerBeregningMangelfultGrunnlag

--- src/main/kotlin/no/nav/pensjon/regler/to/BeregnOpptjeningResponse.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.pensjon.regler.to
-
-
-import no.nav.pensjon.regler.domain.grunnlag.Pensjonsbeholdning
-
-@Deprecated("Avvikles. Bruk BeregnPensjonsBeholdningResponse.", ReplaceWith("no.nav.pensjon.regler.to.BeregnPensjonsBeholdningResponse"))
-class BeregnOpptjeningResponse : ServiceResponse() {
-    var beholdninger: ArrayList<Pensjonsbeholdning> = ArrayList()
-}
