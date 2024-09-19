db9e29a8eaeda4e2b883bb09e4ea75b9c41b6a35
ehellerslien <erik.hellerslien@nav.no>
2024-09-19 11:03:12 +0200

    Endrer sistRegulert: LocalDate til sistRegulertG: Int

--- src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/AfpOffentligLivsvarigGrunnlag.kt
+++ src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/AfpOffentligLivsvarigGrunnlag.kt
@@ -2,4 +2,4 @@ package no.nav.pensjon.regler.domain.beregning2011
 
 import java.time.LocalDate
 
-data class AfpOffentligLivsvarigGrunnlag(val sistRegulert: LocalDate, val bruttoPerAr: Double)
+data class AfpOffentligLivsvarigGrunnlag(val sistRegulertG: Int, val bruttoPerAr: Double)
