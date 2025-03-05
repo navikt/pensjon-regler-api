36ccab2c52556531f20b36a68c4763aff875a410
tordbjorn.wang.eriksen <tordbjorn.wang.eriksen@nav.no>
2025-03-05 10:43:53 +0100

    PL-7984
    - Fjernet arv fra ServiceRequest/ServiceResponse
    - Bruk av dataclass
    - Bumped versjon til 1.1.3

--- src/main/kotlin/no/nav/pensjon/regler/to/HentDelingstallResponse.kt
+++ src/main/kotlin/no/nav/pensjon/regler/to/HentDelingstallResponse.kt
@@ -2,7 +2,7 @@ package no.nav.pensjon.regler.to
 
 import no.nav.pensjon.regler.domain.sats.delingstall.Delingstall
 
-class HentDelingstallResponse : ServiceResponse() {
-    var arskull: Long = 0
-    var delingstall: List<Delingstall> = emptyList()
-}
+data class HentDelingstallResponse (
+    val arskull: Long,
+    val delingstall: List<Delingstall>
+)
