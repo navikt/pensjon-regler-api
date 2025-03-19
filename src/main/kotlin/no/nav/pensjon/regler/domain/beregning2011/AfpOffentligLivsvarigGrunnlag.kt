249d97b970ecf7b6974ed32ae5cf60da6d3aa1e0
ehellerslien <erik.hellerslien@nav.no>
2025-03-19 08:02:09 +0100

    Legger på default null på uttaksgrad

--- src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/AfpOffentligLivsvarigGrunnlag.kt
+++ src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/AfpOffentligLivsvarigGrunnlag.kt
@@ -2,4 +2,4 @@ package no.nav.pensjon.regler.domain.beregning2011
 
 import java.time.LocalDate
 
-data class AfpOffentligLivsvarigGrunnlag(val sistRegulertG: Int, val bruttoPerAr: Double, val uttaksdato: LocalDate?)
+data class AfpOffentligLivsvarigGrunnlag(val sistRegulertG: Int, val bruttoPerAr: Double, val uttaksdato: LocalDate? = null)
