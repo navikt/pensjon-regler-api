974c6fbc0cfe501ff9a91395e235738dda60de58
ehellerslien <erik.hellerslien@nav.no>
2025-06-03 09:32:22 +0200

    Default konstruktør for BeregnAfpEtteroppgjorRequest og KategoriserAfpEtteroppgjorRequest

--- src/main/kotlin/no/nav/pensjon/regler/to/BeregnAfpEtteroppgjorRequest.kt
+++ src/main/kotlin/no/nav/pensjon/regler/to/BeregnAfpEtteroppgjorRequest.kt
@@ -3,11 +3,11 @@ package no.nav.pensjon.regler.to
 import no.nav.pensjon.regler.domain.afpoppgjor.AFPEtteroppgjorgrunnlag
 import java.util.*
 
-class BeregnAfpEtteroppgjorRequest : ServiceRequest() {
+class BeregnAfpEtteroppgjorRequest(
     @Deprecated(
         "Avvikles, erstattes av 'afpEtteroppgjorgrunnlag'",
         replaceWith = ReplaceWith("afpEtteroppgjorgrunnlag")
     )
-    var afpEtteroppgjorgrunnlagListe = Vector<AFPEtteroppgjorgrunnlag>()
+    var afpEtteroppgjorgrunnlagListe: Vector<AFPEtteroppgjorgrunnlag> = Vector<AFPEtteroppgjorgrunnlag>(),
     var afpEtteroppgjorgrunnlag: AFPEtteroppgjorgrunnlag? = null
-}
+) : ServiceRequest()
