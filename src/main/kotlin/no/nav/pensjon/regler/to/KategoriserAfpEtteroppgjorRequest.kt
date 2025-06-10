e750e203e886ef1d9b43a94155213d6b2a1cd5e8
ehellerslien <erik.hellerslien@nav.no>
2025-06-10 07:12:28 +0200

    Fjernet deprekerte felter for AfpEtteroppgjor

--- src/main/kotlin/no/nav/pensjon/regler/to/KategoriserAfpEtteroppgjorRequest.kt
+++ src/main/kotlin/no/nav/pensjon/regler/to/KategoriserAfpEtteroppgjorRequest.kt
@@ -1,13 +1,7 @@
 package no.nav.pensjon.regler.to
 
 import no.nav.pensjon.regler.domain.afpoppgjor.AFPEtteroppgjorgrunnlag
-import java.util.*
 
 class KategoriserAfpEtteroppgjorRequest(
-    @Deprecated(
-        "Avvikles, erstattes av 'afpEtteroppgjorgrunnlag'",
-        replaceWith = ReplaceWith("afpEtteroppgjorgrunnlag")
-    )
-    var afpEtteroppgjorgrunnlagListe: Vector<AFPEtteroppgjorgrunnlag>? = Vector<AFPEtteroppgjorgrunnlag>(),
     var afpEtteroppgjorgrunnlag: AFPEtteroppgjorgrunnlag? = null
 ) : ServiceRequest()
