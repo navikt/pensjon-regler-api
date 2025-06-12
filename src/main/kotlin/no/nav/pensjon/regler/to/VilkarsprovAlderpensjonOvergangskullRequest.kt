3cded6844ae8cf4a89c94bd02543e934de241510
ehellerslien <erik.hellerslien@nav.no>
2025-06-12 14:55:35 +0200

    Fjerner utilsiktet serializable annotasjon

--- src/main/kotlin/no/nav/pensjon/regler/to/VilkarsprovAlderpensjonOvergangskullRequest.kt
+++ src/main/kotlin/no/nav/pensjon/regler/to/VilkarsprovAlderpensjonOvergangskullRequest.kt
@@ -1,12 +1,10 @@
 package no.nav.pensjon.regler.to
 
-import kotlinx.serialization.Serializable
 import no.nav.pensjon.regler.domain.beregning2011.AfpPrivatLivsvarig
 import no.nav.pensjon.regler.domain.beregning2011.SisteAldersberegning2011
 import no.nav.pensjon.regler.domain.krav.Kravhode
 import java.util.*
 
-@Serializable
 class VilkarsprovAlderpensjonOvergangskullRequest : ServiceRequest() {
     var kravHode: Kravhode? = null
     var virkFom: Date? = null
