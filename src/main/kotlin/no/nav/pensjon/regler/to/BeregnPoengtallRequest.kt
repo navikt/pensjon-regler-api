29cc4db8b9ec4fa26f3b8d9eaff0162896c3bfec
Jens.Schiefloe <jens.schiefloe@nav.no>
2026-01-14 06:32:33 +0100

    Release v1.2.5: Slettet request/response for tjeneste BeregnPoengtall som ikke har endepunkt.

--- src/main/kotlin/no/nav/pensjon/regler/to/BeregnPoengtallRequest.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.pensjon.regler.to
-
-import no.nav.pensjon.regler.domain.grunnlag.Opptjeningsgrunnlag
-import java.util.*
-
-class BeregnPoengtallRequest : ServiceRequest() {
-    var opptjening: Opptjeningsgrunnlag? = null
-    var fodselsdato: Date? = null
-}
