77010ca2e1fcc02b1cfd95246cc6deae6ecd4062
Torstein Nesby <ttnesby@gmail.com>
2026-02-17 11:08:16 +0100

    minimum BeslutningsstotteBeregnIFURequest

--- src/main/kotlin/no/nav/pensjon/regler/to/BeslutningsstotteBeregnIFURequest.kt
+++ src/main/kotlin/no/nav/pensjon/regler/to/BeslutningsstotteBeregnIFURequest.kt
@@ -1,11 +1,19 @@
 package no.nav.pensjon.regler.to
 
 import no.nav.pensjon.regler.domain.enum.BorMedTypeEnum
+import no.nav.pensjon.regler.domain.vedtak.InntektForUforhet
+import no.nav.pensjon.regler.domain.vedtak.Uforetidspunkt
+import no.nav.pensjon.regler.domain.vedtak.UngUfor
 import no.nav.pensjon.regler.domain.vedtak.VilkarsVedtak
 
 class BeslutningsstotteBeregnIFURequest(
-    /** Vilkårsvedtaket for uføretrygd. Påkrevd. */
-    var vilkarsvedtak: VilkarsVedtak
+    /**
+     * Fra siste beregningsvilkar periode:
+     */
+    var inntektForUforhet: InntektForUforhet,
+    var ungUfor: UngUfor,
+    var uforetidspunkt: Uforetidspunkt
+
 ) : ServiceRequest() {
     /** Vurdert sivilstand. Valgfri — brukes for å fastsette regnesSomGift. */
     var benyttetSivilstandEnum: BorMedTypeEnum? = null
