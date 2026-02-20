c5ae66cb8193a697d5f4c7f5b5e2ef6f403f8939
Torstein Nesby <ttnesby@gmail.com>
2026-02-20 14:01:36 +0100

    PL-8185, må ha virkFom ift ny faktorerer 202610, nytt regelsett

--- src/main/kotlin/no/nav/pensjon/regler/to/BeslutningsstotteBeregnIFURequest.kt
+++ src/main/kotlin/no/nav/pensjon/regler/to/BeslutningsstotteBeregnIFURequest.kt
@@ -4,7 +4,7 @@ import no.nav.pensjon.regler.domain.enum.BorMedTypeEnum
 import no.nav.pensjon.regler.domain.vedtak.InntektForUforhet
 import no.nav.pensjon.regler.domain.vedtak.Uforetidspunkt
 import no.nav.pensjon.regler.domain.vedtak.UngUfor
-import no.nav.pensjon.regler.domain.vedtak.VilkarsVedtak
+import java.util.*
 
 class BeslutningsstotteBeregnIFURequest(
     /**
@@ -17,4 +17,7 @@ class BeslutningsstotteBeregnIFURequest(
 ) : ServiceRequest() {
     /** Vurdert sivilstand. Valgfri — brukes for å fastsette regnesSomGift. */
     var benyttetSivilstandEnum: BorMedTypeEnum? = null
+
+    /** Virkningsdato fom, fra vilkarsvedtak */
+    var virkFom: Date? = null
 }
