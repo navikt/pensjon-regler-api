94f0332016bf7757fd3eeeae598f6ab5f18f7946
Torstein Nesby <ttnesby@gmail.com>
2026-04-07 08:51:03 +0200

    skal ha fom og tom

--- src/main/kotlin/no/nav/pensjon/regler/to/IdentifiserRegelendringerRequest.kt
+++ src/main/kotlin/no/nav/pensjon/regler/to/IdentifiserRegelendringerRequest.kt
@@ -1,6 +1,5 @@
 package no.nav.pensjon.regler.to
 
-import com.fasterxml.jackson.annotation.JsonAlias
 import no.nav.pensjon.regler.domain.krav.Kravhode
 import no.nav.pensjon.regler.domain.vedtak.VilkarsVedtak
 import java.util.*
@@ -31,17 +30,14 @@ class IdentifiserRegelendringerRequest : ServiceRequest() {
     /**
      * Fra og med dato for regelendringer.
      */
-    @Deprecated("Use virkLd instead")
-    @JsonAlias("fom")
-    var virk: Date? = null
-    @JsonAlias("fomLd")
-    var virkLd: LocalDate? = null
+    @Deprecated("Bruk fomLd")
+    var fom: Date? = null
+    var fomLd: LocalDate? = null
 
     /**
      * Til og med dato for regelendringer. Ved uviss tom-dato kan dagens dato benyttes.
      */
     @Deprecated("Use tomLd instead")
-    @JsonAlias("tom")
     var tom: Date? = null
     var tomLd: LocalDate? = null
 }
