3230a467d5c0790d0916b4f58bb7464ec68aa8e2
Joakim Jacobsen <joakim.jacobsen@nav.no>
2026-05-28 15:35:39 +0200

    Endringer på request og response for beslutningsstøtte tjeneste for beregning av IEU for jan2026-tilfeller

--- src/main/kotlin/no/nav/pensjon/regler/to/BeslutningsstotteBeregnIEUForJan2026Response.kt
+++ src/main/kotlin/no/nav/pensjon/regler/to/BeslutningsstotteBeregnIEUForJan2026Response.kt
@@ -1,8 +1,10 @@
 package no.nav.pensjon.regler.to
 
 import no.nav.pensjon.regler.domain.vedtak.InntektEtterUforhet
+import no.nav.pensjon.regler.domain.vedtak.InntektForUforhet
 
 class BeslutningsstotteBeregnIEUForJan2026Response(
     val ieuOppjustert: Boolean,
-    val inntektEtterUforhet: InntektEtterUforhet
+    val inntektEtterUførhet: InntektEtterUforhet,
+    val inntektFørUførhet: InntektForUforhet,
 ) : ServiceResponse()
