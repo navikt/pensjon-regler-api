4ac1941dcdcd6e39a6171060beb1fce3a3ed2abb
JoakimEJacobsen <62988882+JoakimEJacobsen@users.noreply.github.com>
2026-05-29 12:03:31 +0200

    Fix/beslutningsstotte beregn ieufor jan2026 externals (#141)
    Co-authored-by: Jonas.Hurv.Larsen <jonas.hurv.larsen@nav.no>

--- src/main/kotlin/no/nav/pensjon/regler/to/BeslutningsstotteBeregnIEUForJan2026Request.kt
+++ src/main/kotlin/no/nav/pensjon/regler/to/BeslutningsstotteBeregnIEUForJan2026Request.kt
@@ -1,13 +1,17 @@
 package no.nav.pensjon.regler.to
 
+import no.nav.pensjon.regler.domain.enum.BorMedTypeEnum
 import no.nav.pensjon.regler.domain.vedtak.InntektEtterUforhet
 import no.nav.pensjon.regler.domain.vedtak.InntektForUforhet
 import no.nav.pensjon.regler.domain.vedtak.Uforegrad
+import no.nav.pensjon.regler.domain.vedtak.UngUfor
 import java.time.LocalDate
 
 class BeslutningsstotteBeregnIEUForJan2026Request(
-    val IFUvedVirk: InntektForUforhet,
+    val inntektFørUførhet: InntektForUforhet,
     val virk: LocalDate,
-    val tidligereUføregrad: Uforegrad,
-    val tidligereFastsattIEU: InntektEtterUforhet
+    val uføregrad: Uforegrad,
+    val inntektEtterUførhet: InntektEtterUforhet,
+    val ungUfør: UngUfor,
+    val benyttetSivilstand: BorMedTypeEnum,
 ) : ServiceRequest()
