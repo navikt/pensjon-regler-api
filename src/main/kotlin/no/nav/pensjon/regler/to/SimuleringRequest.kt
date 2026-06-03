7cb46e941f51ba9bce860afbfd094c2fb7e3ec86
Erik.Hellerslien <erik.hellerslien@nav.no>
2026-06-03 11:42:05 +0200

    Fjerner deprecated dato-konstruktør for SimuleringRequest

--- src/main/kotlin/no/nav/pensjon/regler/to/SimuleringRequest.kt
+++ src/main/kotlin/no/nav/pensjon/regler/to/SimuleringRequest.kt
@@ -10,22 +10,6 @@ class SimuleringRequest() : ServiceRequest() {
     var beregnForsorgingstillegg = false
     var beregnInstitusjonsopphold = false
 
-    @Deprecated("Use fomLd-constructor instead")
-    constructor(
-        simulering: Simulering?,
-        // fom: Date?,
-        ektefelleMottarPensjon: Boolean,
-        beregnForsorgingstillegg: Boolean,
-        beregnInstitusjonsopphold: Boolean
-    ) : this() {
-        this.simulering = simulering
-//        this.fom = fom
-        this.ektefelleMottarPensjon = ektefelleMottarPensjon
-        this.beregnForsorgingstillegg = beregnForsorgingstillegg
-        this.beregnInstitusjonsopphold = beregnInstitusjonsopphold
-    }
-
-
     constructor(
         simulering: Simulering?,
         fom: LocalDate?,
