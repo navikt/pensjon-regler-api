b4868c6c81533b03a600a369b2992e73b7300917
Erik.Hellerslien <erik.hellerslien@nav.no>
2026-06-03 11:41:21 +0200

    Fjerner ubrukt felt infoPavirkendeYtelse på BeregnAlderspensjon2025ForsteUttakRequest

--- src/main/kotlin/no/nav/pensjon/regler/to/BeregnAlderspensjon2025ForsteUttakRequest.kt
+++ src/main/kotlin/no/nav/pensjon/regler/to/BeregnAlderspensjon2025ForsteUttakRequest.kt
@@ -2,18 +2,15 @@ package no.nav.pensjon.regler.to
 
 import no.nav.pensjon.regler.domain.beregning2011.AfpOffentligLivsvarigGrunnlag
 import no.nav.pensjon.regler.domain.beregning2011.AfpPrivatLivsvarig
-import no.nav.pensjon.regler.domain.grunnlag.InfoPavirkendeYtelse
 import no.nav.pensjon.regler.domain.krav.Kravhode
 import no.nav.pensjon.regler.domain.vedtak.VilkarsVedtak
-import java.util.*
 import java.time.LocalDate
+import java.util.*
 
 class BeregnAlderspensjon2025ForsteUttakRequest : ServiceRequest() {
     var virkFomLd: LocalDate? = null
     var kravhode: Kravhode? = null
     var vilkarsvedtakListe: List<VilkarsVedtak> = Vector()
-    @Deprecated("Felt er ikke i bruk.")
-    var infoPavirkendeYtelse: InfoPavirkendeYtelse? = null
     var epsMottarPensjon = false
     var afpPrivatLivsvarig: AfpPrivatLivsvarig? = null
     var afpOffentligLivsvarigGrunnlag: AfpOffentligLivsvarigGrunnlag? = null
