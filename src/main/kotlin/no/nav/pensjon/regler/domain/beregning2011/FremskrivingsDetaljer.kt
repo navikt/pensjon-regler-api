783a707789fee039b2758f71d6426046ed5bd0ed
Erik.Hellerslien <erik.hellerslien@nav.no>
2026-06-03 11:40:45 +0200

    Fjerner resterende Date-felt

--- src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/FremskrivingsDetaljer.kt
+++ src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/FremskrivingsDetaljer.kt
@@ -1,12 +1,9 @@
 package no.nav.pensjon.regler.domain.beregning2011
 
 import java.io.Serializable
-import java.util.*
 import java.time.LocalDate
 
 class FremskrivingsDetaljer : Serializable, IFremskriving {
-    @Deprecated("Use justeringTomDatoLd instead")
-    // override var justeringTomDato: Date? = null
     override var justeringTomDatoLd: LocalDate? = null
     override var justeringsfaktor = 0.0
     override var teller = 0.0
@@ -15,9 +12,6 @@ class FremskrivingsDetaljer : Serializable, IFremskriving {
 
     constructor()
     constructor(frem: FremskrivingsDetaljer) : this() {
-//        if (frem.justeringTomDato != null) {
-//            justeringTomDato = frem.justeringTomDato!!.clone() as Date
-//        }
         if(frem.justeringTomDatoLd != null) {
             justeringTomDatoLd = frem.justeringTomDatoLd
         }
--- src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/LonnsvekstDetaljer.kt
+++ src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/LonnsvekstDetaljer.kt
@@ -1,12 +1,9 @@
 package no.nav.pensjon.regler.domain.beregning2011
 
 import java.io.Serializable
-import java.util.*
 import java.time.LocalDate
 
 class LonnsvekstDetaljer : Serializable, ILonnsvekst {
-    @Deprecated("Use justeringTomDatoLd instead")
-    // override var justeringTomDato: Date? = null
     override var justeringTomDatoLd: LocalDate? = null
     override var justeringsfaktor = 0.0
     override var lonnsvekst = 0.0
@@ -14,9 +11,6 @@ class LonnsvekstDetaljer : Serializable, ILonnsvekst {
     constructor()
 
     constructor(lvd: LonnsvekstDetaljer) : this() {
-//        if (lvd.justeringTomDato != null) {
-//            justeringTomDato = lvd.justeringTomDato!!.clone() as Date
-//        }
         if(lvd.justeringTomDatoLd != null) {
             justeringTomDatoLd = lvd.justeringTomDatoLd
         }
@@ -25,10 +19,8 @@ class LonnsvekstDetaljer : Serializable, ILonnsvekst {
     }
 
     constructor(
-        // justeringTomDato: Date? = null,
         justeringsfaktor: Double = 0.0,
         lonnsvekst: Double = 0.0) {
-//        this.justeringTomDato = justeringTomDato
         this.justeringsfaktor = justeringsfaktor
         this.lonnsvekst = lonnsvekst
     }
