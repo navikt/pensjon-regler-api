8ed3c1cf57ca2a44699025a1118c6dda5cf7fa6e
Jens.Schiefloe <jens.schiefloe@nav.no>
2026-08-17 15:12:41 +0200

    PL-8358: Inntektsavkortning uføretrygd med fribeløp beregnet med vektet sats fom 2026.

--- src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/FribeløpPeriode.kt
+++ src/main/kotlin/no/nav/pensjon/regler/domain/beregning2011/FribeløpPeriode.kt
@@ -2,15 +2,35 @@ package no.nav.pensjon.regler.domain.beregning2011
 
 import no.nav.pensjon.regler.domain.enum.VenteperiodeBegrunnelseEnum
 import java.time.LocalDate
-import java.time.temporal.ChronoUnit
 
 class FribeløpPeriode(
+    /**
+     * Fra hvilken måned i virkåret perioden gjelder fra og med.
+     */
     val fom: LocalDate,
+
+    /**
+     * Fra hvilken måned i virkåret perioden gjelder til og med.
+     */
     var tom: LocalDate,
+
+    /**
+     * Hvilken faktor perioden bidrar med ved beregning av fribeløp.
+     */
     val faktor: Double,
+
+    /**
+     * Grunnbeløpet som legges til grunn for perioden.
+     */
     val grunnbeløp: Int,
+
+    /**
+     * Fra hvilken dato venteperioden har startet å telle fra.
+     */
     val venteperiodeStartDato: LocalDate,
-    val venteperiodeBegrunnelse: VenteperiodeBegrunnelseEnum
-) {
-    fun lengde(): Int = ChronoUnit.MONTHS.between(fom, tom.withDayOfMonth(1)).toInt() + 1
-}
+
+    /**
+     * Årsaken til hvorfor venteperioden har startet.
+     */
+    val venteperiodeBegrunnelseEnum: VenteperiodeBegrunnelseEnum
+)
